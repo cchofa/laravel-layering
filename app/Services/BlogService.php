c9c77a9d8294adbfd535b2ac1cc9bd94bc7be390
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-10-12 09:58:31 +0700

    fix bug

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -11,41 +11,41 @@ use DomDocument;
 
 class BlogService
 {
-	private $blogRepository;
+    private $blogRepository;
 
-	public function __construct(BlogRepositoryInterface $blogRepository)
-	{
-		$this->blogRepository = $blogRepository;
-	}
+    public function __construct(BlogRepositoryInterface $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
 
-	public function getAllData()
-	{
-		return $this->blogRepository->getAll();
-	}
+    public function getAllData()
+    {
+        return $this->blogRepository->getAll();
+    }
 
-	public function getPaginateData($limit)
-	{
-		return $this->blogRepository->getPaginate($limit);
-	}
+    public function getPaginateData($limit)
+    {
+        return $this->blogRepository->getPaginate($limit);
+    }
 
-	public function getByIdData($id)
-	{
-		return $this->blogRepository->getById($id);
-	}
+    public function getByIdData($id)
+    {
+        return $this->blogRepository->getById($id);
+    }
 
-	public function getBySlugData($slug)
-	{
-		return $this->blogRepository->getBySlug($slug);
-	}
+    public function getBySlugData($slug)
+    {
+        return $this->blogRepository->getBySlug($slug);
+    }
 
-	public function createData($payload)
-	{
-		/**
-		 * Upload thumbnail
-		 */
+    public function createData($payload)
+    {
+        /**
+         * Upload thumbnail
+         */
         $file = $payload->file('thumbnail');
-        $before_thumbnail_filename = Str::lower(Str::random(9).$file->getClientOriginalName());
-        $after_thumbnail_filename = Str::replace([' ','-','_','(',')','%'], '', $before_thumbnail_filename);
+        $before_thumbnail_filename = Str::lower(Str::random(9) . $file->getClientOriginalName());
+        $after_thumbnail_filename = Str::replace([' ', '-', '_', '(', ')', '%'], '', $before_thumbnail_filename);
         $file->storeAs('public/uploads/images/blogs', $after_thumbnail_filename);
 
         /**
@@ -55,15 +55,15 @@ class BlogService
         $dom->loadHtml($payload->body, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
         $images = $dom->getElementsByTagName('img');
 
-        foreach($images as $k => $img){
+        foreach ($images as $k => $img) {
             $data = $img->getAttribute('src');
             list($type, $data) = explode(';', $data);
             list(, $data)      = explode(',', $data);
             $data = base64_decode($data);
 
-            $image_name = "/storage/uploads/images/blogs/". Str::random(9).time().$k.'.png';
-            $path = public_path().$image_name;
-            File::put($path,$data);
+            $image_name = "/storage/uploads/images/blogs/" . Str::random(9) . time() . $k . '.png';
+            $path = public_path() . $image_name;
+            File::put($path, $data);
 
             $img->removeAttribute('src');
             $img->setAttribute('src', $image_name);
@@ -79,30 +79,30 @@ class BlogService
             'body' => $content,
         ];
 
-		return $this->blogRepository->create($payload);
-	}
+        return $this->blogRepository->create($payload);
+    }
 
-	public function updateData($id, $payload)
-	{
-		return $this->blogRepository->update($id, $payload);
-	}
+    public function updateData($id, $payload)
+    {
+        return $this->blogRepository->update($id, $payload);
+    }
 
-	public function deleteData($id)
-	{
-		$blog = $this->blogRepository->getById($id);
+    public function deleteData($id)
+    {
+        $blog = $this->blogRepository->getById($id);
 
-		Storage::delete('public/uploads/images/blogs/'.$blog->thumbnail);
+        Storage::delete('public/uploads/images/blogs/' . $blog->thumbnail);
 
-		$content = $blog->body;
-		$dom = new DomDocument();
+        $content = $blog->body;
+        $dom = new DomDocument();
         $dom->loadHtml($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
         $images = $dom->getElementsByTagName('img');
 
-		foreach ($images as $img) {
+        foreach ($images as $img) {
             $data = $img->getAttribute('src');
             File::delete(public_path($data));
         }
 
-		return $this->blogRepository->delete($id);
-	}
+        return $this->blogRepository->delete($id);
+    }
 }
